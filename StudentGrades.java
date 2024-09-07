0512d9322e10e0196ab2cb1ca94c1d73c5ff7692
AJAY <124550111+193ajay@users.noreply.github.com>
2024-09-07 19:39:49 +0530

    Update StudentGrades.java

--- StudentGrades.java
+++ StudentGrades.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class StudentGrades {
     public static void main(String[] args) {
-        // Create a Scanner object for input
+        
         Scanner scanner = new Scanner(System.in);
         
-        // Create an ArrayList to store student grades
+        
         ArrayList<Integer> grades = new ArrayList<>();
         
-        // Get the number of students
+        
         System.out.print("Enter the number of students: ");
         int numStudents = scanner.nextInt();
         
-        // Get the grades for each student
+        
         for (int i = 0; i < numStudents; i++) {
             System.out.print("Enter grade for student " + (i + 1) + ": ");
             int grade = scanner.nextInt();
